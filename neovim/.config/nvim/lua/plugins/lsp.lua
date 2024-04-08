7f9e200faec8df0e6300fa00672f3774850b3aef
Antun Badurina <antun.badurina@gmail.com>
2024-04-08 22:17:40 +0200

    Fix prettier formatting

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -142,6 +142,8 @@ return {
   },
   {
     'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
     opts = {
       notify_on_error = false,
       format_on_save = function()
@@ -150,11 +152,16 @@ return {
           lsp_fallback = true,
         }
       end,
+      formatters = {
+        prettier = {
+          prepend_args = { '--config', '.prettierrc' },
+        },
+      },
       formatters_by_ft = {
         lua = { 'stylua' },
         javascript = { 'prettier' },
         typescriptreact = { 'prettier' },
-        typescripty = { 'prettier' },
+        typescript = { 'prettier' },
       },
     },
   },
