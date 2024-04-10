22622ba244aba3d2181a20eb6954e0a5c301639a
Antun Badurina <antun.badurina@gmail.com>
2024-04-10 23:01:41 +0200

    Fix colorscheme for Neogit

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,16 +3,16 @@ return {
     'mcchrish/zenbones.nvim',
     priority = 1000,
     init = function()
-      local auto_dark_mode = require 'auto-dark-mode'
+      vim.cmd.colorscheme 'neobones'
 
+      local auto_dark_mode = require 'auto-dark-mode'
       auto_dark_mode.setup {
         update_interval = 1000,
         set_dark_mode = function()
           vim.api.nvim_set_option('background', 'dark')
-          vim.cmd.colorscheme 'kanagawabones'
         end,
         set_light_mode = function()
-          vim.cmd.colorscheme 'neobones'
+          vim.api.nvim_set_option('background', 'light')
         end,
       }
     end,
