868ce749486c5ba541ea540a83061191a35f70d8
Antun Badurina <antun.badurina@gmail.com>
2024-04-13 20:18:52 +0200

    Change harpoon keymap descriptions

--- neovim/.config/nvim/lua/plugins/harpoon.lua
+++ neovim/.config/nvim/lua/plugins/harpoon.lua
@@ -9,46 +9,46 @@ return {
 
       vim.keymap.set('n', '<leader>ha', function()
         harpoon:list():add()
-      end, { desc = 'Harpoon add' })
+      end, { desc = 'Add' })
 
       -- Remove
       vim.keymap.set('n', '<leader>hr', function()
         harpoon:list():remove()
-      end, { desc = 'Harpoon remove' })
+      end, { desc = 'Remove' })
 
       -- Clear
       vim.keymap.set('n', '<leader>hc', function()
         harpoon:list():clear()
-      end, { desc = 'Harpoon clear' })
+      end, { desc = 'Clear' })
 
       -- Harpoon menu
       vim.keymap.set('n', '<C-h>', function()
         harpoon.ui:toggle_quick_menu(harpoon:list())
-      end)
+      end, { desc = 'Harpoon list' })
 
       -- Previous
       vim.keymap.set('n', '<leader>hp', function()
         harpoon:list():prev()
-      end, { desc = 'Harpoon previous' })
+      end, { desc = 'Previous' })
 
       -- Next
       vim.keymap.set('n', '<leader>hn', function()
         harpoon:list():next()
-      end, { desc = 'Harpoon next' })
+      end, { desc = 'Next' })
 
       -- Quick select
       vim.keymap.set('n', '<leader>hh', function()
         harpoon:list():select(1)
-      end)
+      end, { desc = 'Select 1' })
       vim.keymap.set('n', '<leader>hj', function()
         harpoon:list():select(2)
-      end)
+      end, { desc = 'Select 2' })
       vim.keymap.set('n', '<leader>hk', function()
         harpoon:list():select(3)
-      end)
+      end, { desc = 'Select 3' })
       vim.keymap.set('n', '<leader>hl', function()
         harpoon:list():select(4)
-      end)
+      end, { desc = 'Select 4' })
     end,
   },
 }
