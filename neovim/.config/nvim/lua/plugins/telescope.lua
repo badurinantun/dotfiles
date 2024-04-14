78a6c5f7a40cd029681cc61c884038864706b599
Antun Badurina <antun.badurina@gmail.com>
2024-04-14 23:56:03 +0200

    Adjust Telescope keymaps

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -32,11 +32,18 @@ return {
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Find files' })
       vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Buffers' })
-      vim.keymap.set('n', '<leader>d', builtin.diagnostics, { desc = 'Diagnostic' })
+      vim.keymap.set('n', '<leader>dd', builtin.diagnostics, { desc = 'Diagnostics' })
       vim.keymap.set('n', '<leader>K', builtin.keymaps, { desc = 'Keymaps' })
       vim.keymap.set('n', '<leader>/', builtin.live_grep, { desc = 'Live grep' })
       vim.keymap.set('n', '<leader>:', builtin.commands, { desc = 'Commands' })
-      vim.keymap.set('n', '<leader>?', builtin.help_tags, { desc = 'Commands' })
+      vim.keymap.set('n', '<leader>?', builtin.help_tags, { desc = 'Help tags' })
+
+      vim.keymap.set('n', '<leader>s', function()
+        local word = vim.fn.expand '<cword>'
+        builtin.grep_string {
+          word = word,
+        }
+      end, { desc = 'Find word' })
 
       vim.keymap.set('n', '<leader>F', function()
         builtin.find_files {
