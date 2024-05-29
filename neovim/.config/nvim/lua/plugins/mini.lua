18913141eeb56af014dc8520fdb910d2bb5a124b
Antun <antun@pangolin.home>
2024-05-29 23:16:29 +0200

    Change mini status line

--- neovim/.config/nvim/lua/plugins/mini.lua
+++ neovim/.config/nvim/lua/plugins/mini.lua
@@ -1,29 +1,5 @@
 local utils = require 'utils'
 
-local CTRL_S = vim.api.nvim_replace_termcodes('<C-S>', true, true, true)
-local CTRL_V = vim.api.nvim_replace_termcodes('<C-V>', true, true, true)
-
-local modes = setmetatable({
-  ['n'] = { long = 'Normal', short = 'NOR', hl = 'MiniStatuslineModeNormal' },
-  ['v'] = { long = 'Visual', short = 'VIS', hl = 'MiniStatuslineModeVisual' },
-  ['V'] = { long = 'V-Line', short = 'V-L', hl = 'MiniStatuslineModeVisual' },
-  [CTRL_V] = { long = 'V-Block', short = 'V-B', hl = 'MiniStatuslineModeVisual' },
-  ['s'] = { long = 'Select', short = 'SEL', hl = 'MiniStatuslineModeVisual' },
-  ['S'] = { long = 'S-Line', short = 'S-L', hl = 'MiniStatuslineModeVisual' },
-  [CTRL_S] = { long = 'S-Block', short = 'S-B', hl = 'MiniStatuslineModeVisual' },
-  ['i'] = { long = 'Insert', short = 'INS', hl = 'MiniStatuslineModeInsert' },
-  ['R'] = { long = 'Replace', short = 'RPL', hl = 'MiniStatuslineModeReplace' },
-  ['c'] = { long = 'Command', short = 'CMD', hl = 'MiniStatuslineModeCommand' },
-  ['r'] = { long = 'Prompt', short = '-P-', hl = 'MiniStatuslineModeOther' },
-  ['!'] = { long = 'Shell', short = 'SH-', hl = 'MiniStatuslineModeOther' },
-  ['t'] = { long = 'Terminal', short = 'TER', hl = 'MiniStatuslineModeOther' },
-}, {
-  -- By default return 'Unknown' but this shouldn't be needed
-  __index = function()
-    return { long = 'Unknown', short = 'U', hl = '%#MiniStatuslineModeOther#' }
-  end,
-})
-
 local function isnt_normal_buffer()
   return vim.bo.buftype ~= ''
 end
@@ -73,31 +49,9 @@ return {
       end, { desc = 'Close all buffers' })
 
       local statusline = require 'mini.statusline'
-      statusline.setup { use_icons = false }
+      statusline.setup { use_icons = true }
 
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_mode = function()
-        local mode_info = modes[vim.fn.mode()]
-        local mode = mode_info.short
-        return mode, mode_info.hl
-      end
 
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_git = function()
-        if isnt_normal_buffer() then
-          return ''
-        end
-        --
-        ---@diagnostic disable: undefined-field
-        local head = vim.b.gitsigns_head or ''
-
-        return string.format('%s', head)
-      end
-
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_diagnostics = function()
-        return ''
-      end
 
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_fileinfo = function()
