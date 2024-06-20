307c90879db78ecc6e13a8fbb8401ace87e366ac
Antun Badurina <antun.badurina@gmail.com>
2024-06-20 11:15:26 +0200

    Update to v0.10.0

--- neovim/.config/nvim/lua/plugins/git.lua
+++ neovim/.config/nvim/lua/plugins/git.lua
@@ -13,11 +13,10 @@ return {
   },
   {
     'NeogitOrg/neogit',
-    tag = 'v0.0.1',
     dependencies = {
       'nvim-lua/plenary.nvim', -- required
-      'sindrets/diffview.nvim', -- optional - Diff integration
-      'nvim-telescope/telescope.nvim', -- optional
+      'sindrets/diffview.nvim', -- diff integration
+      'nvim-telescope/telescope.nvim',
     },
     config = function()
       local neogit = require 'neogit'
