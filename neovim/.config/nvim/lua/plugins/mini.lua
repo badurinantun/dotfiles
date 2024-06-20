673b6cae36a1b9f0c5bd0e24a5f0ee9986017f3a
Antun Badurina <antun.badurina@gmail.com>
2024-06-20 16:25:51 +0200

    Fix clue

--- neovim/.config/nvim/lua/plugins/mini.lua
+++ neovim/.config/nvim/lua/plugins/mini.lua
@@ -73,7 +73,7 @@ return {
             { mode = 'n', keys = '<Leader>h', desc = 'Harpoon' },
             { mode = 'n', keys = '<Leader>n', desc = 'Neorg' },
             { mode = 'n', keys = '<Leader>d', desc = 'Document' },
-            { mode = 'n', keys = '<Leader>n', desc = 'Project' },
+            { mode = 'n', keys = '<Leader>p', desc = 'Project' },
           },
           miniclue.gen_clues.builtin_completion(),
           miniclue.gen_clues.g(),
