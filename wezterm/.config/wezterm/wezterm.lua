79637878b0227145b632f588e3da24e24deedc37
Antun Badurina <antun.badurina@gmail.com>
2024-06-20 14:48:10 +0200

    Switch theme base on system

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -11,7 +11,7 @@ config.tab_max_width = 64
 config.use_fancy_tab_bar = false
 config.switch_to_last_active_tab_when_closing_tab = true
 config.show_new_tab_button_in_tab_bar = false
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false
 config.tab_bar_at_bottom = false
 
 -- window
@@ -24,6 +24,24 @@ config.color_schemes = {
   ['zenwritten_light'] = require('color_schemes.zenwritten_light').scheme,
   ['zenwritten_dark'] = require('color_schemes.zenwritten_dark').scheme,
 }
+
+local function scheme_for_appearance(appearance)
+  if appearance:find 'Light' then
+    return 'zenwritten_light'
+  else
+    return 'zenwritten_dark'
+  end
+end
+
+wezterm.on('window-config-reloaded', function(window)
+  local overrides = window:get_config_overrides() or {}
+  local appearance = window:get_appearance()
+  local scheme = scheme_for_appearance(appearance)
+  if overrides.color_scheme ~= scheme then
+    overrides.color_scheme = scheme
+    window:set_config_overrides(overrides)
+  end
+end)
 
 -- keys
 config.disable_default_key_bindings = false
