c79066d4c75bfd0b5e093a55bed3976a0580b2aa
Antun Badurina <antun.badurina@gmail.com>
2025-08-07 22:37:46 +0200

    Add some plugins

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -4,9 +4,6 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Disable terminal GUI colors
-vim.opt.termguicolors = false
-
 -- Hide number
 vim.opt.number = false
 
@@ -28,6 +25,9 @@ vim.opt.scrolloff = 10
 -- Make cursor block
 vim.opt.guicursor = ''
 
+-- Always show signcolumn
+vim.opt.signcolumn = 'yes'
+
 -- Hightlight search
 vim.opt.hlsearch = true
 
@@ -37,9 +37,6 @@ vim.opt.wildoptions = 'fuzzy'
 -- Hidden
 vim.opt.hidden = false
 
--- Decrese mapped sequence wait time
-vim.opt.timeoutlen = 300
-
 -- Disable mouse
 vim.opt.mouse = ''
 
@@ -48,22 +45,92 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 
-
--- Netrw
-vim.g.netrw_banner = 0
-vim.g.netrw_liststyle = 3
+-- Disable Netrw
+vim.g.loaded_netrw = 1
 
 --[[ KEYMAPS ]]
-
-vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = 'Close' })
 
 -- Clear search highlights
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
--- Explorer
-vim.keymap.set('n', '<leader>e', ':Ex<CR>', { desc = 'Explorer' })
+vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = 'Close' })
+vim.keymap.set('n', '<leader>e', ':lua MiniFiles.open()<CR>', { desc = 'Explorer' })
+vim.keymap.set('n', '<leader>p', ':Pick files<CR>', { desc = 'Project files' })
+vim.keymap.set('n', '<leader>b', ':Pick buffers<CR>', { desc = 'Buffers' })
+vim.keymap.set('n', '<leader>h', ':Pick help<CR>', { desc = 'Help' })
+vim.keymap.set('n', '<leader>f', ':Pick grep_live<CR>', { desc = 'Grep' })
 
+--[[ PLUGINS ]]
 
+local path_package = vim.fn.stdpath('data') .. '/site/'
+local mini_path = path_package .. 'pack/deps/start/mini.deps'
+if not vim.loop.fs_stat(mini_path) then
+	vim.cmd('echo "Installing `mini.nvim`" | redraw')
+	local clone_cmd = {
+		'git', 'clone', '--filter=blob:none',
+		'https://github.com/echasnovski/mini.deps', mini_path
+	}
+	vim.fn.system(clone_cmd)
+	vim.cmd('packadd mini.deps | helptags ALL')
+	vim.cmd('echo "Installed `mini.deps`" | redraw')
+end
 
+local MiniDeps = require('mini.deps')
 
+MiniDeps.setup({ path = { package = path_package } })
+
+local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
+
+now(function()
+	add({ source = 'echasnovski/mini.notify', checkout = 'stable' })
+	add({ source = 'echasnovski/mini.ai', checkout = 'stable' })
+	add({ source = 'echasnovski/mini.comment', checkout = 'stable' })
+	add({ source = 'echasnovski/mini.pick', checkout = 'stable' })
+	add({ source = 'echasnovski/mini.surround', checkout = 'stable' })
+	add({ source = 'echasnovski/mini.files', checkout = 'stable' })
+
+	require('mini.notify').setup()
+	vim.notify = require('mini.notify').make_notify()
+end)
+
+later(function()
+	vim.cmd('colorscheme quiet')
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "CursorLine", { link = "Visual" })
+end)
+
+later(function()
+	require('mini.ai').setup()
+	require('mini.comment').setup()
+	require('mini.pick').setup()
+	require('mini.files').setup()
+	require('mini.surround').setup()
+
+	vim.api.nvim_set_hl(0, "MiniPickBorder", { bg = "none" })
+	vim.api.nvim_set_hl(0, "MiniFilesBorder", { bg = "none" })
+end)
+
+later(function()
+	add({ source = 'nvim-treesitter/nvim-treesitter',
+		-- Use 'master' while monitoring updates in 'main'
+		checkout = 'master',
+		monitor = 'main',
+		hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
+	})
+	require('nvim-treesitter.configs').setup({
+		ensure_installed = { 'lua', 'vimdoc' },
+		auto_install = true,
+		highlight = { enable = true },
+	})
+end)
+
+later(function()
+	add({source = 'nvim-orgmode/orgmode', checkout = 'master'})
+
+	require('orgmode').setup({
+      org_agenda_files = '~/orgfiles/**/*',
+      org_default_notes_file = '~/orgfiles/refile.org',
+    })
+end)
 
